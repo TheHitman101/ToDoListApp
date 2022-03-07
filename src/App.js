c50e57460a9f612c3c04d626a3c968bb6b7a5068
TheHitman101 <69303402+TheHitman101@users.noreply.github.com>
2022-03-07 17:27:14 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {useState} from 'react';
+
+
+function App() {
+  
+  const [value, setValue] = useState();
+;
+function setValueAsNum(){
+  setValue(value);
+}
+  
+  return (
+    <div className="App">
+       <input type ="text" onChange={setValueAsNum}/>
+
+    </div>
+  );
+}
+
+export default App;
